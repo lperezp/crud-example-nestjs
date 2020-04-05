b92f62ca1031375d814c8287f4b5a170c4161bb7
lperezp <luiseperezp.10@gmail.com>
2020-04-05 03:17:56 -0500

    feat(Auth): Add validation in password with hash

--- src/modules/auth/repository/user.repository.ts
+++ src/modules/auth/repository/user.repository.ts
@@ -6,25 +6,35 @@ import {
   ConflictException,
 } from '@nestjs/common';
 import { StatusCode } from 'src/core/enum/statusCode.enum';
-
+import * as bcrypt from 'bcrypt';
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  // https://github.com/kelektiv/node.bcrypt.js#readme
   async singUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
     const { username, password } = authCredentialsDTO;
 
     const user = new User();
+    /*
+     * Usamos  la funcion de genSalt para obtener bits aleatorios y lo
+     * guardamos en la dB como una columna
+     */
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
     try {
       await user.save();
     } catch (error) {
-      console.log('s', StatusCode.DUPLICATE_CODE);
       if (error.code === StatusCode.DUPLICATE_CODE) {
         throw new ConflictException('Username already exists.');
       } else {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  // agregamos esta funcion para cifrar el password y la variable salt
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
