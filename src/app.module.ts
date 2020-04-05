6a9e2337f64cf82cecc1cf55268b2c6f35d01e80
lperezp <luiseperezp.10@gmail.com>
2020-04-05 01:23:50 -0500

    feat(Auth): Add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), TasksModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), TasksModule, AuthModule],
 })
 export class AppModule {}
