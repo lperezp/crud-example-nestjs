b06792b775a7594a1d6fe4408cf6e107ce49ac67
lperezp <luiseperezp.10@gmail.com>
2020-04-05 01:10:23 -0500

    (fix): Changed config file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,7 +4,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
   port: 3306,
-  username: 'admin',
+  username: 'root',
   password: '',
   database: 'tasks_management',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
