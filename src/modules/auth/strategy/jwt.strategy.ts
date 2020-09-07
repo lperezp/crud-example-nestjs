c4757e674dabff0030955d3963a9a83b961a6a23
Luis Eduardo <luiseperezp.10@gmail.com>
2020-09-07 09:16:43 -0500

    Update jwt.strategy.ts

--- src/modules/auth/strategy/jwt.strategy.ts
+++ src/modules/auth/strategy/jwt.strategy.ts
@@ -23,7 +23,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   async validate(payload: JwtPayload): Promise<User> {
     const { username } = payload;
     const user = this.userRepository.findOne({ username });
-
     if (!user) {
       throw new UnauthorizedException();
     } else {
